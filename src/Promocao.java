d640bb76742291b50ec9c18ef8a2a3d72c043c6e
brunosequeira <uc2020235721@student.uc.pt>
2021-11-16 22:04:10 +0000

    mudanca de 1

---
+++ src/Promocao.java
@@ -0,0 +1,8 @@
+public class Promocao {
+    private int num; // nnum = 0, um == 1, pague menos, nuum ==2 ahd
+    private Data inicio;
+    private Data fim;
+    public Promocao(){
+        Data inicio = new Data(12,12,12);
+    }
+}
---
+++ src/Venda.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Venda {
+    private Cliente cli;
+    private ArrayList<Produto> prods = new ArrayList<>();
+    public Venda(Cliente nov, Produto novP) {
+        this.cli = nov;
+
+    }
+}
