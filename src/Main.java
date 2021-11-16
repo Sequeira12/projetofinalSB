5cfabad51a5706defced08006d98aab94cf969ec
brunosequeira <uc2020235721@student.uc.pt>
2021-11-16 22:07:22 +0000

    mudei a main so para testar

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
        
         System.out.println("Hello World!");
+        Aplicacao n = new Aplicacao();
+        // se o samu está a ler isto ta bem
+
     }
 }
